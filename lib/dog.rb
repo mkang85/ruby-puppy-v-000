41ad0cc41c4a235512708a09cd635722b3d3917c
Michael Kang <michaelkang85@gmail.com>
2018-02-10 19:01:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,4 +4,14 @@ class Dog
   def self.clear_all
     @@all = []
   end
+
+  def self.all
+    @@all.each do |ele|
+      puts ele
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
 end
